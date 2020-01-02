a61a0693c2f5208ff79f8d6be5bfd83b8eebb199
Anastasia Gerzha <33519170+lupwue@users.noreply.github.com>
2020-01-02 16:16:24 +0200

    Add files via upload

--- src/mx_mem_two_dim.c
+++ src/mx_mem_two_dim.c
@@ -3,13 +3,13 @@
 char **mx_mem_two_dim(const char *s, char c) {
     int words = mx_count_words(s, c);
     char **arr = (char **)malloc(sizeof(char *) * (words + 1));
+    int countsym = 0;
 
-    for (int i = 0; s[i]; i++) {
+    for (int i = 0, j = 0; s[i]; i++) {
         if (s[i] != c) {
-            for (int countsym = 0, j = 0; s[i] != c && s[i]; i++) {
+            for (countsym = 0; s[i] != c && s[i]; i++)   
                 countsym++;
                 arr[j++] = mx_strnew(countsym);
-            }
         }
     }
     return arr ? arr : NULL;
